e086a01ce5cc22f133247a2175216a66a688ea3d
Mohammed-Aftab-Siddique <aftabsd786@gmail.com>
2026-06-24 15:44:54 +0000

    Test Dynatrace

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from TheCodingFeast!"
+    return "Hello from TheCodingFeast to Dynatrace!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
